3f681dc56ba154572ac6948cb46e0f39b0edc676
Ryan Emberling <itsalaidbacklife@gmail.com>
2019-02-17 22:07:54 -0500

    Added classes for questions and answers. Pause video and play when answer is hit

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ console.log(firstScriptTag);
 console.log(firstScriptTag.parentNode);
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+
+
+let playerWidth = 640;
+let playerHeight = 390;
+
 let player;
+let currentQuestion = -1; //index of which question is currently displayed
+let pausedForQuestion = false; //whether video is paused while student answers question
+
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
-		height: '390',
-		width: '640',
+		height: playerHeight,
+		width: playerWidth,
 		videoId: 'WYcw_DcZsak',
 		host: 'https://www.youtube.com',
 		events: {
@@ -30,14 +38,15 @@ function onPlayerReady(event){
 }
 
 
-let done = false;
 // Fires when video begins/pauses
 	// Used to pause video at specified time(s)
 function onPlayerStateChange(event) {
 	console.log("State change");
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-		setTimeout(pauseVideo, 5000);
-		done = true;
+	if (event.data == YT.PlayerState.PLAYING) {
+		if (currentQuestion < questions.length - 1) {
+			currentQuestion++;
+			setTimeout(setQuestion, questions[currentQuestion].time * 1000, currentQuestion, questions);
+		}
 	}
 }
 
@@ -45,4 +54,43 @@ function pauseVideo() {
 	player.pauseVideo();
 }
 
+function Question(time, duration, answers) {
+	this.time = time;
+	this.duration = duration;
+	this.answers = answers;
+}
+
+function Answer(text, correct, feedback) {
+	this.text = text;
+	this.correct = correct;
+	this.feedback = feedback;
+}
+
+function setQuestion(index, questions) {
+	player.pauseVideo();
+	question = questions[index];
+
+	let parent = "#answersRowOne";
+	question.answers.forEach((answer, i) => {
+		var row
+		if (i > 1) {
+			parent = "#answersRowTwo";
+		}
+		$('<button/>', {
+			class: "answer",
+			text: answer.text
+		}).on('click', ev => {
+			player.playVideo();
+			$('.answersRow').empty();
+		}).appendTo(parent);
+	});
+
+
+}
+
+
+
+let q1 = new Question(2, 5, [new Answer("Radient heat for a wire wrapped around a mica sheet", true, "You go it!"), new Answer("Direct heat from a wire wrapped around a mica sheet", false, "Nice Try")]);
+let q2 = new Question(3, 5, [new Answer("Because I said so", false, "It is just!"), new Answer("Because it is just", true, "Indeed."), new Answer("Maybe not after all", false, "too bad"), new Answer("Lorem Ipsum", false, "this is feedback")]);
+let questions = [q1, q2];
 	
